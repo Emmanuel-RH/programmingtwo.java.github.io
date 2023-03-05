5c652634ed6945c500972abb3e31faa3f94c1285
Emmanuel Reyes <emmanuelrh666@gmail.com>
2023-03-05 17:50:53 -0600

    update

--- parcial1/Trece.java
+++ parcial1/Trece.java
@@ -2,47 +2,69 @@ package parcial1;
 import java.util.Scanner;
 
 public class Trece {
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner input = new Scanner (System.in);
-    
-        System.out.println ( "Inserte sueldo del trabajador:");
-        int Sueldo = input.nextInt(); 
+        System.out.print("Ingrese el precio por hora trabajada: ");
+        double PH = sc.nextDouble();
 
-        System.out.println ( "Inserte horas trabajadas:");
-        int HT = input.nextInt(); 
+        System.out.print("Ingrese las horas trabajadas: ");
+        int HT = sc.nextInt();
 
-        System.out.println("Si cuenta con un credito Infonavit");
+        double ST = 0;
 
-        if (HT<=40) { 
-
-            System.out.println("El Sueldo total es: " + (Sueldo*HT));  
-          
+        if (HT <= 40) {
+            ST = PH * HT;
         } 
+        else if (HT <= 60) {
+            int horasNormales = 40;
+            int horasExtra = HT - horasNormales;
+            double precioHoraExtra = PH * 1.5;
 
-        else if (HT<=60) {           
+            ST = (PH * horasNormales) + (precioHoraExtra * horasExtra);
+        }
+        else{
+            int horasmax = 20;
+            double precioHoraExtra = PH * 1.5;
+
+            ST = (horasmax * precioHoraExtra + (PH * 40));
+            System.out.println("Excediste el máximo de horas extra, solo se te pagará el máximo (20)");
+        }
+
+        double descuentoInfonavit = 0;
+        System.out.print("¿El trabajador tiene casa propia? (Si/No): ");
+        String respuesta = sc.next();
+
+        if (respuesta.equalsIgnoreCase("No")) {
+            descuentoInfonavit = ST * 0.1;
+        } 
+        else if (respuesta.equalsIgnoreCase("Si")) {
+            descuentoInfonavit = ST * 0.25;
+        }
+
+        double PensionAlim = 0.00;
+        System.out.print("¿Paga pensión alimenticia? (Si/No): ");
+        String respuesta1 = sc.next();
+
+        if (respuesta1.equalsIgnoreCase("No")) {
+            PensionAlim = 0;
+        } 
+        else if (respuesta1.equalsIgnoreCase("Si")) {
             
-            int HE = HT - 40;
-            double SalarioHE = 1.5;
-            double SalarioComp = HE * Sueldo * SalarioHE;
-            int resultado = Sueldo * 40;
+            System.out.print("Introduza el descuento de su Pensión: ");
+            double varia = sc.nextDouble();
 
-            System.out.println("Las horas extras son: " + HE );
-            System.out.println("El sueldo es: " + (SalarioComp +  resultado));
-        } 
+            PensionAlim = (ST * varia) / 100.00;
 
-        else {
+        }
 
-            System.out.println("Excediste las horas extra." );
-            int HE = 20;
-            double SalarioHE = 1.5;
-            double SalarioComp = HE * Sueldo * SalarioHE;
-            int resultado = Sueldo * 40;
+        double sueldoTotal = ST - (descuentoInfonavit + PensionAlim);
 
-            System.out.println("Las horas extras son: " + HE );
-            System.out.println("El sueldo es: " + (SalarioComp +  resultado));
-        } 
+        System.out.println("Sueldo del trabajador: $" + ST);
+        System.out.println("Descuento por Infonavit: $" + descuentoInfonavit);
+        System.out.println("Descuento por Pensión Alimenticia: $" + PensionAlim);
+        System.out.println("Sueldo total: $" + sueldoTotal);
 
-        input.close(); 
+        sc.close();
     }
 }
