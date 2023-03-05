9e955b8668f995c50ab6d2012a56c595902f9b9a
Emmanuel Reyes <emmanuelrh666@gmail.com>
2023-03-05 17:50:47 -0600

    new update .java

--- parcial1/Catorce.java
+++ parcial1/Catorce.java
@@ -1,17 +1,31 @@
 package parcial1;
-import java.util.Scanner;
+import java.util.Scanner; 
 
-public class Catorce {
-    public static void main (String[] args){
+public class Catorce { 
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in); 
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Inserte el sueldo del trabajador: ");
-        int Sueldo = sc.nextInt();
-
-        System.out.println("Introduzca las horas trabajadas diarias: ");
-        int HT = sc.nextInt();
-
-        if ()
-    }
-}
+        System.out.println ( "Inserte pago por hora:");
+        int PH = input.nextInt(); 
+        
+        { 
+        double flotante1 = 1.00;
+        double resul1 = PH / flotante1;
+        System.out.println("El pago por hora del trabajador americano es: " + resul1 );
+        } 
+        
+        { 
+        double flotante2 = 0.8;
+        double resul2 = PH * flotante2;
+        System.out.println("El pago por hora del trabajador residente es: " + resul2 );
+        } 
+        
+        { 
+        double flotante3 = 0.6;
+        double resul3 = PH * flotante3;
+        System.out.println("El pago por hora del trabajador ilegal es: " + resul3 );
+        } 
+        
+        input.close(); 
+    } 
+} 
