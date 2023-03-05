f0dce382cae38ac7592bd112f88a4f51cc739a1b
Emmanuel Reyes <emmanuelrh666@gmail.com>
2023-03-05 12:37:29 -0600

    new update .java

---
+++ parcial1/Catorce.java
@@ -0,0 +1,17 @@
+package parcial1;
+import java.util.Scanner;
+
+public class Catorce {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Inserte el sueldo del trabajador: ");
+        int Sueldo = sc.nextInt();
+
+        System.out.println("Introduzca las horas trabajadas diarias: ");
+        int HT = sc.nextInt();
+
+        if ()
+    }
+}
---
+++ parcial1/Trece.java
@@ -0,0 +1,48 @@
+package parcial1;
+import java.util.Scanner;
+
+public class Trece {
+    public static void main (String[] args){
+
+        Scanner input = new Scanner (System.in);
+    
+        System.out.println ( "Inserte sueldo del trabajador:");
+        int Sueldo = input.nextInt(); 
+
+        System.out.println ( "Inserte horas trabajadas:");
+        int HT = input.nextInt(); 
+
+        System.out.println("Si cuenta con un credito Infonavit");
+
+        if (HT<=40) { 
+
+            System.out.println("El Sueldo total es: " + (Sueldo*HT));  
+          
+        } 
+
+        else if (HT<=60) {           
+            
+            int HE = HT - 40;
+            double SalarioHE = 1.5;
+            double SalarioComp = HE * Sueldo * SalarioHE;
+            int resultado = Sueldo * 40;
+
+            System.out.println("Las horas extras son: " + HE );
+            System.out.println("El sueldo es: " + (SalarioComp +  resultado));
+        } 
+
+        else {
+
+            System.out.println("Excediste las horas extra." );
+            int HE = 20;
+            double SalarioHE = 1.5;
+            double SalarioComp = HE * Sueldo * SalarioHE;
+            int resultado = Sueldo * 40;
+
+            System.out.println("Las horas extras son: " + HE );
+            System.out.println("El sueldo es: " + (SalarioComp +  resultado));
+        } 
+
+        input.close(); 
+    }
+}
